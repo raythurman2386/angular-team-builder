8644e5981c25e00f856061092f58847a0319db92
raythurman2386 <raymondthurman5@gmail.com>
2020-02-18 22:43:43 -0500

    app and nav component, no real changes as of now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  // { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+  // { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+  // { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
